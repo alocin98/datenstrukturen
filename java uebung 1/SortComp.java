165f1d9fc21995dfbb25a0db5378ddb7130670bf
Nicolas Müller <nicolasmuller@stud-184-234.eduroam.unibe.ch>
2018-02-26 15:34:51 +0100

    hallo

---
+++ java uebung 1/SortComp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SortComp{
+
+  public static void main (String[] args){
+    System.out.println("Array: " + Arrays.toString(createArray(100)));
+  }
+
+  private static int[] createArray(int length){
+    Random random = new Random();
+    int[] array = new int[length];
+    for(int i = 0; i < array.length; i++){
+      array[i] = random.nextInt(length);
+    }
+    return array;
+  }
+
+
+
+}
