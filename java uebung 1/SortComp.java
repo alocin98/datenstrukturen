4df6949d0634b0c86bd36811bed93deaf1c77ee8
Cedric Aebi <cedric.aebi1@students.unibe.ch>
2018-02-26 21:33:11 +0100

    bla

--- java uebung 1/SortComp.java
+++ java uebung 1/SortComp.java
@@ -3,18 +3,24 @@ import java.util.*;
 public class SortComp{
 
   public static void main (String[] args){
-    System.out.println("Array: " + Arrays.toString(createArray(100)));
+    System.out.println(createArray(10));
   }
 
   private static int[] createArray(int length){
     Random random = new Random();
     int[] array = new int[length];
     for(int i = 0; i < array.length; i++){
-      array[i] = random.nextInt(length);
+      array[i] = random.nextInt(100) + 1;
     }
     return array;
   }
 
+  private static long insertTime(){
+    
+  }
 
+  private static long mergeTime(){
+
+  }
 
 }
