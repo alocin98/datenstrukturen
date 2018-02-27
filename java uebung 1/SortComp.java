7d5d62c96babe7cc71c825344b09d2bd8b6d567d
Cedric Aebi <cedric.aebi1@students.unibe.ch>
2018-02-27 09:15:13 +0100

    finished. wenn wettsch chame schüsch no a de zahle umespiele

--- java uebung 1/SortComp.java
+++ java uebung 1/SortComp.java
@@ -2,8 +2,17 @@ import java.util.*;
 
 public class SortComp{
 
+  static Timer time = new Timer();
+
   public static void main (String[] args){
-    System.out.println(insertionTime(100000));
+    int[] elements = {2000, 5000, 8000, 10000, 20000, 50000, 80000, 100000,
+                      200000, 500000, 800000};
+    for(int i = 0; i <= 10; i++){
+      System.out.println("Test " + i + " insertionSort with " + elements[i] +
+      " elements took " + insertionTime(elements[i]) + " ms");
+      System.out.println("Test " + i + " MergeSort with " + elements[i] +
+      " elements took " + mergeTime(elements[i]) + "ms\n");
+    }
   }
 
   private static int[] createArray(int length){
@@ -17,14 +26,15 @@ public class SortComp{
 
   private static long insertionTime(int length){
     int[] array = createArray(length);
-    Timer time = new Timer();
     time.reset();
     Sorting.insertionSort(array);
     return time.timeElapsed();
   }
 
-/*  private static long mergeTime(){
-
+  private static long mergeTime(int length){
+    int[] array = createArray(length);
+    time.reset();
+    Sorting.mergeSort(array, 0, array.length-1);
+    return time.timeElapsed();
   }
-*/
 }
