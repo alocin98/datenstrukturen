1e7aec67e249588bd18da6aeb1cc5478639bc428
Nicolas Mueller <nicolas.mueller@students.unibe.ch>
2018-03-12 11:07:33 +0100

    NameVorname Klasse und Methoden definiert

---
+++ uebung_3/java/NameVornameComparator.java
@@ -0,0 +1,21 @@
+/**
+* Vergleicht Studenten anhand von Name und Vorname.
+*/
+
+public class NameVornameComparator implements java.util.comparator<StudentIn>{
+  private String namevornameA, namevornameB;
+  /**
+  * liefert:    -1  a ist vor b im Alphabet
+  *             0   a ist gleich b
+  *             1   b ist vor a im Alphabet
+  * --> Name vor Vorname, also z.b Mueller Nicolas
+  */
+  public int compare(StudentIn a, StudentIn b) {
+    namevornameA = combineNames(a);
+    namevornameB = combineNames(b);
+  }
+
+  private String combineNames(StudentIn std){
+    return std.getName().concat(std.getVorname).trim().toLowerCase();
+  }
+}
