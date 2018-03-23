3b2f29c594c9b9756fdcf397cfef58df3cb9b965
Nicolas Mueller <nicolas.mueller@students.unibe.ch>
2018-03-23 09:52:59 +0100

    asdf

--- uebung_4/java/RadixSort.java
+++ uebung_4/java/RadixSort.java
@@ -4,25 +4,25 @@ public class RadixSort {
 
 	/*
 	 * Implements radix sort. Character arrays of different lengths
-	 * are ordered lexicographically, for example, a<ab<b. The 
-	 * implementation doesn't use counting sort as a stable sort. 
-	 * Instead, it simply uses an array of queues for each character 
-	 * value.  
-	 * 
+	 * are ordered lexicographically, for example, a<ab<b. The
+	 * implementation doesn't use counting sort as a stable sort.
+	 * Instead, it simply uses an array of queues for each character
+	 * value.
+	 *
 	 * @param A an array of character arrays with different lengths
-	 * @param d the length of the longest array in A 
+	 * @param d the length of the longest array in A
 	 */
 	public static void radixSort(char[][] A, int d)
 	{
 		// 27 queues for 26 characters plus 'empty' character
 		LinkedList[] queues = new LinkedList[27];
-		
+
 		// for all positions from right to left
 		for(int j=d-1; j>=0; j--)
 		{
 			// initialize empty queues
 			for(int i=0; i<27; i++) queues[i] = new LinkedList();
-			
+
 			// place each character array in correct queue
 			for(int i=0; i<A.length; i++)
 			{
@@ -30,7 +30,7 @@ public class RadixSort {
 				{
 					// characters 'a'-'z'
 					queues[A[i][j]-'a'+1].addLast(A[i]);
-				} 
+				}
 				else
 				{
 					// character array is shorter than current position.
@@ -39,16 +39,16 @@ public class RadixSort {
 					queues[0].addLast(A[i]);
 				}
 			}
-			
-			// traverse queues 
+
+			// traverse queues
 			int n = 0;
 			for(int i=0; i<27; i++)
-			{				
-				while(queues[i].size() > 0) 
+			{
+				while(queues[i].size() > 0)
 				{
 					A[n] = (char[])queues[i].removeFirst();
 					n++;
-				} 
+				}
 			}
 		}
 	}
